52055fb6936b49a46836c0639f10957fc67c3265
PotatoBlaster <PotatoBlaster@users.noreply.github.com>
2018-06-29 20:01:31 +0800

    added things
    yes i know my summaries are shit

--- jagaitech.py
+++ jagaitech.py
@@ -6,6 +6,7 @@ from discord.ext.commands import Bot
 import asyncio
 import time
 import math
+import random
 
 bot = commands.Bot(command_prefix='.')
 
@@ -33,6 +34,22 @@ async def on_ready():
     global rrphost
     global arpdoc
     global rrpdoc
+    global cqrphost
+    global cqrpstarted
+    global cqrptimestarted
+    global cqrpdoc
+    global tnrrphost
+    global tnrrpstarted
+    global tnrrptimestarted
+    global tnrrpdoc
+    global dndrphost
+    global dndrpstarted
+    global dndrptimestarted
+    global dndrpdoc
+    global kdmrphost
+    global kdmrpstarted
+    global kdmrptimestarted
+    global kdmrpdoc
     arp = None
     rrp = None
     arpstarted = False
@@ -43,6 +60,54 @@ async def on_ready():
     rrphost = None
     arpdoc = None
     rrpdoc = None
+    cqrphost = None
+    cqrpstarted = False
+    cqrptimestarted = None
+    cqrpdoc = None
+    tnrrphost = None
+    tnrrpstarted = False
+    tnrrptimestarted = None
+    tnrrpdoc = None
+    dndrphost = None
+    dndrpstarted = False
+    dndrptimestarted = None
+    dndrpdoc = None
+    kdmrphost = None
+    kdmrpstarted = False
+    kdmrptimestarted = None
+    kdmrpdoc = None
+
+@bot.command(pass_context=True)
+async def say(ctx, *, arg):
+    if str(ctx.message.author.id) == "154552600073601024":
+        await bot.say(arg)
+    else:
+        embed = discord.Embed(title="User is too much of a skrub to use this command.")
+        await bot.say(embed=embed)
+
+@bot.command(aliases=["dice"])
+async def roll(num):
+    num = int(num)
+    if num < 1 or num > 10000:
+        embed = discord.Embed(title="Please enter a number between 1 and 10000.")
+        await bot.say(embed=embed)
+        return
+    embed = discord.Embed(title=random.randint(1,num))
+    await bot.say(embed=embed)
+
+@bot.command(aliases=["choose"])
+async def pick(*, arg):
+    args = arg.split(",")
+    if len(args) == 1:
+        embed = discord.Embed(title="Please provide more than one choice.")
+        await bot.say(embed=embed)
+        return
+    if len(args) > 10:
+        embed = discord.Embed(title="Please provide less than ten options.")
+        await bot.say(embed=embed)
+        return
+    embed = discord.Embed(title=random.choice(args))
+    await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["rpset"])
 @commands.has_any_role("Staff","Voice")
@@ -67,6 +132,18 @@ async def start(ctx):
     global rrpstarted
     global arptimestarted
     global rrptimestarted
+    global cqrphost
+    global cqrpstarted
+    global cqrptimestarted
+    global tnrrphost
+    global tnrrpstarted
+    global tnrrptimestarted
+    global dndrphost
+    global dndrpstarted
+    global dndrptimestarted
+    global kdmrphost
+    global kdmrpstarted
+    global kdmrptimestarted
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="No RP has been set.")
@@ -93,7 +170,58 @@ async def start(ctx):
         rrptimestarted = time.time()
         embed = discord.Embed(title="The RP has started.")
         await bot.say(embed=embed)
-
+    if ctx.message.channel.name == "conquest":
+        if cqrphost == None:
+            embed = discord.Embed(title="Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        if cqrpstarted:
+            embed = discord.Embed(title="The RP has already started.")
+            await bot.say(embed=embed)
+            return
+        cqrpstarted = True
+        cqrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has started.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        if tnrrphost == None:
+            embed = discord.Embed(title="Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        if tnrrpstarted:
+            embed = discord.Embed(title="The RP has already started.")
+            await bot.say(embed=embed)
+            return
+        tnrrpstarted = True
+        tnrrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has started.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        if dndrphost == None:
+            embed = discord.Embed(title="Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        if dndrpstarted:
+            embed = discord.Embed(title="The RP has already started.")
+            await bot.say(embed=embed)
+            return
+        dndrpstarted = True
+        dndrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has started.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        if kdmrphost == None:
+            embed = discord.Embed(title="Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        if kdmrpstarted:
+            embed = discord.Embed(title="The RP has already started.")
+            await bot.say(embed=embed)
+            return
+        kdmrpstarted = True
+        kdmrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has started.")
+        await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["rp","arpee","host","doc"])
 async def roleplay(ctx):
@@ -107,6 +235,22 @@ async def roleplay(ctx):
     global rrphost
     global arpdoc
     global rrpdoc
+    global cqrphost
+    global cqrpstarted
+    global cqrptimestarted
+    global cqrpdoc
+    global tnrrphost
+    global tnrrpstarted
+    global tnrrptimestarted
+    global tnrrpdoc
+    global dndrphost
+    global dndrpstarted
+    global dndrptimestarted
+    global dndrpdoc
+    global kdmrphost
+    global kdmrpstarted
+    global kdmrptimestarted
+    global kdmrpdoc
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="There is no RP.")
@@ -145,9 +289,78 @@ async def roleplay(ctx):
             if not rrpdoc == None:
                 embed.add_field(name="Doc:",value=rrpdoc,inline=False)
             await bot.say(embed=embed)
+    if ctx.message.channel.name == "conquest":
+        if cqrphost == None:
+            embed = discord.Embed(title="There is no Conquest running.")
+            await bot.say(embed=embed)
+            return
+        if not cqrpstarted:
+            embed = discord.Embed(title="The RP is Conquest. Use .start to start the RP.")
+            embed.add_field(name="Host: ",value=cqrphost)
+            if not cqrpdoc == None:
+                embed.add_field(name="Doc: ",value=cqrpdoc,inline=False)
+            await bot.say(embed=embed)
+        else:
+            embed = discord.Embed(title="The RP is Conquest.",description="In progress for: "+toTime(time.time()-cqrptimestarted))
+            embed.add_field(name="Host: ",value=cqrphost)
+            if not cqrpdoc == None:
+                embed.add_field(name="Doc: ",value=cqrpdoc,inline=False)
+            await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        if tnrrphost == None:
+            embed = discord.Embed(title="There is no Trainer running.")
+            await bot.say(embed=embed)
+            return
+        if not tnrrpstarted:
+            embed = discord.Embed(title="The RP is Trainer. Use .start to start the RP.")
+            embed.add_field(name="Host: ",value=tnrrphost)
+            if not tnrrpdoc == None:
+                embed.add_field(name="Doc: ",value=tnrrpdoc,inline=False)
+            await bot.say(embed=embed)
+        else:
+            embed = discord.Embed(title="The RP is Trainer.",description="In progress for: "+toTime(time.time()-tnrrptimestarted))
+            embed.add_field(name="Host: ",value=tnrrphost)
+            if not tnrrpdoc == None:
+                embed.add_field(name="Doc: ",value=tnrrpdoc,inline=False)
+            await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        if dndrphost == None:
+            embed = discord.Embed(title="There is no Dungeons \'n Dragonites running.")
+            await bot.say(embed=embed)
+            return
+        if not dndrpstarted:
+            embed = discord.Embed(title="The RP is Dungeons \'n Dragonites. Use .start to start the RP.")
+            embed.add_field(name="Host: ",value=dndrphost)
+            if not dndrpdoc == None:
+                embed.add_field(name="Doc: ",value=dndrpdoc,inline=False)
+            await bot.say(embed=embed)
+        else:
+            embed = discord.Embed(title="The RP is Dungeons \'n Dragonites.",description="In progress for: "+toTime(time.time()-dndrptimestarted))
+            embed.add_field(name="Host: ",value=dndrphost)
+            if not dndrpdoc == None:
+                embed.add_field(name="Doc: ",value=dndrpdoc,inline=False)
+            await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        if kdmrphost == None:
+            embed = discord.Embed(title="There is no Kingdom running.")
+            await bot.say(embed=embed)
+            return
+        if not kdmrpstarted:
+            embed = discord.Embed(title="The RP is Kingdom. Use .start to start the RP.")
+            embed.add_field(name="Host: ",value=kdmrphost)
+            if not kdmrpdoc == None:
+                embed.add_field(name="Doc: ",value=kdmrpdoc,inline=False)
+            await bot.say(embed=embed)
+        else:
+            embed = discord.Embed(title="The RP is Kingdom.",description="In progress for: "+toTime(time.time()-kdmrptimestarted))
+            embed.add_field(name="Host: ",value=kdmrphost)
+            if not kdmrpdoc == None:
+                embed.add_field(name="Doc: ",value=kdmrpdoc,inline=False)
+            await bot.say(embed=embed)
+
 
 @bot.command(pass_context=True,aliases=["rpend"])
-@commands.has_any_role("Staff","Voice")
+@commands.has_any_role("Staff")
 async def endrp(ctx):
     global arp
     global rrp
@@ -159,6 +372,22 @@ async def endrp(ctx):
     global rrphost
     global arpdoc
     global rrpdoc
+    global cqrphost
+    global cqrpstarted
+    global cqrptimestarted
+    global cqrpdoc
+    global tnrrphost
+    global tnrrpstarted
+    global tnrrptimestarted
+    global tnrrpdoc
+    global dndrphost
+    global dndrpstarted
+    global dndrptimestarted
+    global dndrpdoc
+    global kdmrphost
+    global kdmrpstarted
+    global kdmrptimestarted
+    global kdmrpdoc
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="There is no RP.")
@@ -187,6 +416,58 @@ async def endrp(ctx):
         rrphost = None
         rrpdoc = None
         await bot.say(embed=embed)
+    if ctx.message.channel.name == "conquest":
+        if cqrphost == None:
+            embed = discord.Embed(title="There is no Conquest running.")
+            await bot.say(embed=embed)
+            return
+        if not cqrpstarted:
+            cqrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has ended.",description="After "+toTime(time.time()-cqrptimestarted))
+        cqrphost = None
+        cqrpstarted = False
+        cqrptimestarted = None
+        cqrpdoc = None
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        if tnrrphost == None:
+            embed = discord.Embed(title="There is no Trainer running.")
+            await bot.say(embed=embed)
+            return
+        if not tnrrpstarted:
+            tnrrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has ended.",description="After "+toTime(time.time()-tnrrptimestarted))
+        tnrrphost = None
+        tnrrpstarted = False
+        tnrrptimestarted = None
+        tnrrpdoc = None
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        if dndrphost == None:
+            embed = discord.Embed(title="There is no Dungeons \'n Dragonites running.")
+            await bot.say(embed=embed)
+            return
+        if not dndrpstarted:
+            dndrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has ended.",description="After "+toTime(time.time()-dndrptimestarted))
+        dndrphost = None
+        dndrpstarted = False
+        dndrptimestarted = None
+        dndrpdoc = None
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        if kdmrphost == None:
+            embed = discord.Embed(title="There is no Kingdom running.")
+            await bot.say(embed=embed)
+            return
+        if not kdmrpstarted:
+            kdmrptimestarted = time.time()
+        embed = discord.Embed(title="The RP has ended.",description="After "+toTime(time.time()-kdmrptimestarted))
+        kdmrphost = None
+        kdmrpstarted = False
+        kdmrptimestarted = None
+        kdmrpdoc = None
+        await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["sh"])
 @commands.has_any_role("Staff","Voice")
@@ -195,6 +476,10 @@ async def sethost(ctx,user:discord.Member):
     global rrp
     global arphost
     global rrphost
+    global cqrphost
+    global tnrrphost
+    global dndrphost
+    global kdmrphost
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="There is no RP.")
@@ -211,6 +496,22 @@ async def sethost(ctx,user:discord.Member):
         rrphost = user.display_name
         embed = discord.Embed(title="The host has been set to "+user.display_name+".")
         await bot.say(embed=embed)
+    if ctx.message.channel.name == "conquest":
+        cqrphost = user.display_name
+        embed = discord.Embed(title="The host has been set to "+user.display_name+".")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        tnrrphost = user.display_name
+        embed = discord.Embed(title="The host has been set to "+user.display_name+".")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        dndrphost = user.display_name
+        embed = discord.Embed(title="The host has been set to "+user.display_name+".")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        kdmrphost = user.display_name
+        embed = discord.Embed(title="The host has been set to "+user.display_name+".")
+        await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["rmh,rmhost"])
 @commands.has_any_role("Staff","Voice")
@@ -219,6 +520,10 @@ async def removehost(ctx):
     global rrp
     global arphost
     global rrphost
+    global cqrphost
+    global tnrrphost
+    global dndrphost
+    global kdmrphost
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="There is no RP.")
@@ -243,6 +548,38 @@ async def removehost(ctx):
         rrphost = None
         embed = discord.Embed(title="The host has been removed.")
         await bot.say(embed=embed)
+    if ctx.message.channel.name == "conquest":
+        if cqrphost == None:
+            embed = discord.Embed(title="There is no host.")
+            await bot.say(embed=embed)
+            return
+        cqrphost = None
+        embed = discord.Embed(title="The host has been removed.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        if tnrrphost == None:
+            embed = discord.Embed(title="There is no host.")
+            await bot.say(embed=embed)
+            return
+        tnrrphost = None
+        embed = discord.Embed(title="The host has been removed.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        if dndrphost == None:
+            embed = discord.Embed(title="There is no host.")
+            await bot.say(embed=embed)
+            return
+        dndrphost = None
+        embed = discord.Embed(title="The host has been removed.")
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        if kdmrphost == None:
+            embed = discord.Embed(title="There is no host.")
+            await bot.say(embed=embed)
+            return
+        kdmrphost = None
+        embed = discord.Embed(title="The host has been removed.")
+        await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["sd"])
 @commands.has_any_role("Staff","Voice")
@@ -251,6 +588,10 @@ async def setdoc(ctx, doc):
     global rrp
     global arpdoc
     global rrpdoc
+    global cqrpdoc
+    global tnrrpdoc
+    global dndrpdoc
+    global kdmrpdoc
     if ctx.message.channel.name == "amphy_rp":
         if arp == None:
             embed = discord.Embed(title="There is no RP.")
@@ -268,6 +609,42 @@ async def setdoc(ctx, doc):
         doc = str(doc)
         rrpdoc = doc
         embed = discord.Embed(title="The doc has been set to "+rrpdoc)
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "conquest":
+        if cqrphost == None:
+            embed = discord.Embed(title="There is no RP running. Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        doc = str(doc)
+        cqrpdoc = doc
+        embed = discord.Embed(title="The doc has been set to "+cqrpdoc)
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "trainer":
+        if tnrrphost == None:
+            embed = discord.Embed(title="There is no RP running. Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        doc = str(doc)
+        tnrrpdoc = doc
+        embed = discord.Embed(title="The doc has been set to "+tnrrpdoc)
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "dungeons_n_dragonites":
+        if dndrphost == None:
+            embed = discord.Embed(title="There is no RP running. Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        doc = str(doc)
+        dndrpdoc = doc
+        embed = discord.Embed(title="The doc has been set to "+dndrpdoc)
+        await bot.say(embed=embed)
+    if ctx.message.channel.name == "kingdom":
+        if kdmrphost == None:
+            embed = discord.Embed(title="There is no RP running. Please set a host first.")
+            await bot.say(embed=embed)
+            return
+        doc = str(doc)
+        kdmrpdoc = doc
+        embed = discord.Embed(title="The doc has been set to "+kdmrpdoc)
         await bot.say(embed=embed)
 
 @bot.command(pass_context=True,aliases=["rmd,rmdoc"])
@@ -302,4 +679,4 @@ async def removedoc(ctx):
         embed = discord.Embed(title="The doc has been removed.")
         await bot.say(embed=embed)
 
-bot.run("insert token")
+bot.run("insert token here")
